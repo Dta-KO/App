4a31cc0d084c7efee85d750bdb69087d2ae42932
DTA_KO <dtako.developer@gmail.com>
2020-02-24 23:49:33 +0700

    change some thing

--- app/src/main/java/com/app/LoginActivity.java
+++ app/src/main/java/com/app/LoginActivity.java
@@ -1,22 +1,35 @@
 package com.app;
 
-import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.tomer.fadingtextview.FadingTextView;
+
+import java.util.Collections;
 
 public class LoginActivity extends AppCompatActivity {
     FadingTextView fadingTextView;
     Button btnLoginByPhoneNumber;
+//    //Dialog
+//    Dialog dialogLogin;
+//    Button btnClear, btnLoginNow;
+//    EditText edtPhone;
+//    TextView txtSuCo;
+
+    //fire base auth
+    FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 101;
 
 
     @Override
@@ -24,13 +37,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
+        mAuth = FirebaseAuth.getInstance();
+
         //init fadingTxt
         fadingTextView = findViewById(R.id.fadingTxt);
         fadingTextView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fadeout));
 
         //init button login phone
         setBtnLoginByPhoneNumber();
-
     }
 
     //init button login phone
@@ -39,15 +53,31 @@ public class LoginActivity extends AppCompatActivity {
         btnLoginByPhoneNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog dialogLogin = new Dialog(LoginActivity.this,R.style.Theme_Dialog);
-                dialogLogin.setContentView(R.layout.dialog_login_tel);
-                Window window = dialogLogin.getWindow();
-                window.setLayout(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-                window.setGravity(Gravity.CENTER);
-                dialogLogin.show();
+                Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                        .setAvailableProviders(Collections.singletonList(new AuthUI.IdpConfig.PhoneBuilder().build()))
+                        .setLogo(R.mipmap.ic_launcher)
+                        .build();
+                startActivityForResult(intent, RC_SIGN_IN);
 
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (mUser != null) {
+                    Log.d("User", mUser.getPhoneNumber());
+                }
+            } else {
+                Log.d("Error", String.valueOf(response.getError()));
+            }
+        }
     }
 }
